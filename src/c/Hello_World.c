fc2dcec24f43db4c207b28980aed203fb0f5450a
Saadat-B <saadatbadgujar@gmail.com>
2025-05-03 07:41:20 +0000

    data types

--- src/c/Hello_World.c
+++ src/c/Hello_World.c
@@ -2,6 +2,16 @@
 
 int main()
 {
-    printf("Starting the Sneklang interpreter\n");
+
+    int num = 1;
+    float fl = 0.1;
+    char egChar = 'c';
+    char *str = "this is a string";
+
+    printf("this is an integer %d\n", num);
+    printf("this is a character %c\n", egChar);
+    printf("this is a string made using char* pointer %s\n", str);
+    printf("this is a float %f\n", fl);
+
     return 0;
 }
