6800459c96017933813c27074c0b15090dbee493
Saadat-B <saadatbadgujar@gmail.com>
2025-05-03 09:44:11 +0000

    for loop

--- src/c/Hello_World.c
+++ src/c/Hello_World.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void printNumbers(int start, int end)
+{
+    if (start > end)
+        return;
+    for (int i = start; i <= end; i++)
+    {
+        printf("%d\n", i);
+    }
+}
 
 int main()
 {
-    printf("sizeof bool is %zu byte\n", sizeof(bool));
-    printf("sizeof int is %zu byte\n", sizeof(int));
-    printf("sizeof float is %zu byte\n", sizeof(float));
-    printf("sizeof double is %zu byte\n", sizeof(double));
-    printf("sizeof size_t is %zu byte\n", sizeof(size_t));
-    printf("sizeof long is %zu byte\n", sizeof(long));
-    return 0;
+    printNumbers(0, 10);
 }
